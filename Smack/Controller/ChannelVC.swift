c9ff12c319c6552f34a76404eedcd57c81c37c68
Ironbatpug <molnarcsaba92@gmail.com>
2019-04-03 20:41:55 +0200

    lesson-02 swreveal bar added

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,17 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Molnár Csaba on 2019. 04. 03..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = 20
+    }
+}
